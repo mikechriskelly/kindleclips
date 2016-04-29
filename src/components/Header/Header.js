798caca51ca5fcf8f68810f6aa3f11f997dc38ac
Mike Kelly <mikechriskelly@gmail.com>
2016-04-28 22:37:10 -0700

    Style header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,19 @@
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Header.scss';
-import Link from '../Link';
 import Navigation from '../Navigation';
-import DebounceInput from 'react-debounce-input';
 
 function Header() {
   return (
     <div className={s.root}>
-      <div className="container">
-        <div className="three columns">
-          <Link className={s.links} to="/random">Random</Link>
-          <Link className={s.links} to="/browse">Browse</Link>
-        </div>
-        <div className="nine columns">
-          <div className={s.inputbar}>
-            <img className={s.searchicon} src="search-icon.svg" alt="" />
-            <DebounceInput
-              className={s.searchfield}
-              debounceTimeout={300}
-              minLength={2}
-              onChange={filterText => this.setState({ filterText })}
-              placeholder="Search..."
-            />
-            <Navigation className={s.nav} />
-          </div>
-        </div>
+      <div className={s.inputbar}>
+        <input
+          className={s.searchfield}
+          onChange={filterText => this.setState({ filterText })}
+          placeholder="Search..."
+        />
       </div>
+      <Navigation className={s.nav} />
     </div>
   );
 }
