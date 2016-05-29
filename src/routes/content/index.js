bbcf79138d27da0af0c28ad446d0a54e3592aee2
Mike Kelly <mikechriskelly@gmail.com>
2016-05-28 18:52:39 -0700

    Cleanup unused pages

--- src/routes/content/index.js
+++ src/routes/content/index.js
@@ -1,19 +1,10 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import React from 'react';
 import Content from './Content';
 import fetch from '../../core/fetch';
 
 export default {
 
-  path: '*',
+  path: '/content',
 
   async action({ path }) { // eslint-disable-line react/prop-types
     const resp = await fetch('/graphql', {
