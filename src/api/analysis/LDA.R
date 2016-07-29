d106e82f03ec4757cbc4b9ca329171e3ab27a24f
ChrisCastle <chris.d.castle@gmail.com>
2016-07-29 12:58:31 -0700

    batch insert function and cleaned vars

--- src/api/analysis/LDA.R
+++ src/api/analysis/LDA.R
@@ -24,10 +24,6 @@ con <- dbConnect(drv, dbname = dbname,
 # query to extract only clips for a single user
 qry <- paste("select id, user_id, hash, title, author, text from clip where user_id =", paste("'", userid, "'", "::uuid", sep = ""))
 tmp <- dbGetQuery(con, qry)
-# dbGetQuery(con, "select title from \"Clip\" where author = 'Seth Godin'")
-
-# extract the entire Clip table, for localhosting only
-# tmp <- dbReadTable(con, "Clip")
 
 # create collapsed author, title, text vector
 clips <- paste(tmp$title, gsub(" ", "", tmp$author), tmp$text)
@@ -43,8 +39,6 @@ dfm <- dfm(clips, toLower = TRUE, removeNumbers = TRUE, removePunct = TRUE
 make_k <- function(tmp) {
       ua <- length(unique(tmp$author))
       ut <- length(unique(tmp$title))
-      # x <- log((ut+ua)^exp(1))
-      # x <- (ua+ut)/16
       x <- (ua ^ 2 + ut ^ 2) ^ (1/4)
       x <- ifelse(x > 25, 25, x)
       x <- ifelse(x < 5, 5, x)
@@ -53,33 +47,35 @@ make_k <- function(tmp) {
 k_tops <- make_k(tmp) # number of topic clusters
 topic_len <- 1:k_tops
 
+# make the LDA model using VEM method, Gibbs provides more even topic distribution
 set.seed(10)
-# lda.gibbs <- LDA(x = dfm, k = k, method = "Gibbs", control = list(verbose = 800, alpha = 50/k, seed = 10)) # make the LDA model
-lda <- LDA(x = dfm, k = k_tops, control = list(verbose = 800, alpha = 50/k_tops, seed = 10))# make the LDA model
-#### method to create gammaDF for new documents using an existing LDA model
+lda <- LDA(x = dfm, k = k_tops, control = list(alpha = 50/k_tops, seed = 10))
+
+#### method to create gamma_df for new documents using an existing LDA model
 # lda_inf <- posterior(ldaModel, NewDoc.dfm)
-# new.gammaDF <- lda_inf$topics
+# new.gamma_df <- lda_inf$topics
 
-# pull topic probs for each clip and put them into tmp list
-gammaDF <- as.data.frame(lda@gamma)
+# pull topic probs for each clip and put them into gamma df
+gamma_df <- as.data.frame(lda@gamma)
+
 # use Hellinger distance for similarity measure of vectors
-distMat <- distHellinger(as.matrix(gammaDF))
-distMat <- distMat * 1000
-
-gammaDF <- round(gammaDF, 6)
+dist_mat <- distHellinger(as.matrix(gamma_df))
+dist_mat <- dist_mat * 1000
+gamma_df <- round(gamma_df, 6)
 
 # create normalized clip similarity table
-distVec <- as.vector(distMat)
-idDistVec <- rep(tmp$id, times = 1, each = NROW(tmp))
-simIdVec <- rep(tmp$id, times = NROW(tmp))
+dist_vec <- as.vector(dist_mat)
+id_dist_vec <- rep(tmp$id, times = 1, each = NROW(tmp))
+sim_id_vec <- rep(tmp$id, times = NROW(tmp))
 clip_dist_table <- data.frame(
-                  "clip_dist_key" = paste(idDistVec, simIdVec, sep = "-"),
-                  "clip_id" = idDistVec,
-                  "clip_dist_id" = simIdVec, 
-                  "distance" = distVec, stringsAsFactors = FALSE)
+                  "clip_dist_key" = paste(id_dist_vec, sim_id_vec, sep = "-"),
+                  "clip_id" = id_dist_vec,
+                  "sim_clip_id" = sim_id_vec, 
+                  "distance" = dist_vec, stringsAsFactors = FALSE)
 rem <- which(clip_dist_table$distance == 0)
 clip_dist_table <- clip_dist_table[-rem, ]
 
+# apply a threshold (90th percentile) to clip_dist and reduce to n top relationships
 make_smaller_table <- function(table, n) {
       thresh <- which(table$distance < quantile(table$distance, 0.1))
       table <- table[thresh, ]
@@ -89,89 +85,61 @@ make_smaller_table <- function(table, n) {
       df
 }
 
-top_10 <- make_smaller_table(clip_dist_table, 10)
+clip_dist_table <- make_smaller_table(clip_dist_table, 10)
 
 # create normalized topic probability table
 topic_prob_table <- data.frame(
-      "topic_prob" = as.vector(t(gammaDF)), # topic probability vector
+      "prob" = as.vector(t(gamma_df)), # topic probability vector
       "topic_id" = rep(topic_len, NROW(tmp)), # repeating topic id vector
       "clip_id" = rep(tmp$id, times = 1, each = k_tops), # repeating clip id vector
       stringsAsFactors = FALSE )
 
-# write a function that will find the top n related clips
-make_tops <- function(tops_id, clip_number, top_n=dim(tops_id)[1]) {
-      tops_id[1:top_n,clip_number]
-}
-
-#===topic naming==
+# topic naming
 # list of collapsed author names
-author.collapse <- tolower(unique(gsub(" ", "", tmp$author)))
+author_collapse <- tolower(unique(gsub(" ", "", tmp$author)))
 
 name_topic <- function(term_length, n_char, top_n_terms) {
-      if (term_length > 80) { print("Watch your back. This might take awhile")}
       term <- terms(lda, term_length) # pull top lda terms
       # using stem completion really slows this function down
       term <- matrix(stemCompletion(as.character(term), dict), nrow = term_length)
       indx <- apply(term, 2, nchar) > n_char 
       term[!indx] <- NA # remove short terms
-      term[term %in% author.collapse] <- NA # remove author names
-      topic.names <- apply(term, 2, FUN = function(x) x[!is.na(x)][1:top_n_terms])
-      topic.names }
+      term[term %in% author_collapse] <- NA # remove author names
+      topic_names <- apply(term, 2, FUN = function(x) x[!is.na(x)][1:top_n_terms])
+      topic_names }
 
-topic.names <- apply(name_topic(35, 6, 3), 2, paste, collapse = '_') # there must be a faster way to do this
-names(topic.names) <- NULL
+topic_names <- apply(name_topic(35, 6, 3), 2, paste, collapse = '_') # there must be a faster way to do this
 
 # create normalized table with topic names
 topic_names_table <- data.frame(
                         "user_id" = tmp$user_id[1], 
                         "topic_id" = topic_len,
-                        "topic_names" = topic.names, 
+                        "name" = topic_names, 
                         stringsAsFactors = FALSE)
 
-# insert user topics into topic table
-txt <- NULL
-for (i in 1:NROW(topic_names_table)) {
-      txt[i] <- paste("('", UUIDgenerate(), "', ",
-                      "'", topic_names_table$user_id[i], "', ",
-                      topic_names_table$topic_id[i], ", ",
-                      "'", topic_names_table$topic_names[i], "',",
-                      "'", Sys.time(), "', ",
-                      "'", Sys.time(), "')",
-                      sep = "")
+make_batch_insert <- function(df, col_1, col_2, col_3, table_name) {
+      txt <- sapply(1:nrow(df), function(x)
+            paste("('", UUIDgenerate(), "',",
+                  "'", df[[col_1]][x], "',",
+                  "'", df[[col_2]][x], "',",
+                  "'", df[[col_3]][x], "',",
+                  "'", Sys.time(), "',",
+                  "'", Sys.time(), "')",
+                  sep = ""))
+      if (class(txt) != 'character') {
+            stop("Output class must be character", call. = FALSE)
+      }
+      txt <- paste(txt, collapse = ", ")
+      txt <- paste("insert into", table_name,  "(id, ", col_1, ",", col_2, ",", col_3, ",", "created_at, updated_at) values", txt)
+      txt
 }
-txt <- paste(txt, collapse = ", ")
-txt <- paste("insert into topic (id, user_id, topic_id, name, created_at, updated_at) values", txt)
-dbGetQuery(con, txt)
 
-# insert into topic_prob table
-txt <- NULL
-for (i in 1:NROW(topic_prob_table)) {
-      txt[i] <- paste("('", UUIDgenerate(), "', ",
-                      "'", topic_prob_table$clip_id[i], "',",
-                      topic_prob_table$topic_id[i], ",",
-                      topic_prob_table$topic_prob[i], ",",
-                      "'", Sys.time(), "', ",
-                      "'", Sys.time(), "')",
-                      sep = "")
-}
-txt <- paste(txt, collapse = ", ")
-txt <- paste("insert into topic_prob (id, clip_id, topic_id, prob, created_at, updated_at) values", txt, sep = "")
-dbGetQuery(con, txt)
+# create the insert into queries
+name_txt <- make_batch_insert(topic_names_table, "user_id", "topic_id", "name", "topic")
+prob_txt <- make_batch_insert(topic_prob_table, "clip_id", "topic_id", "prob", "topic_prob")
+dist_txt <- make_batch_insert(clip_dist_table, "clip_id", "sim_clip_id", "distance", "clip_dist")
 
-clip_dist_table <- top_10 # reduce to the most relevant clips
-# insert into clip distance table
-txt <- NULL
-for (i in 1:NROW(clip_dist_table)) {
-      txt[i] <- paste("('",  UUIDgenerate(), "', ",
-                      "'", clip_dist_table$clip_id[i], "',",
-                      "'", clip_dist_table$clip_dist_id[i], "',",
-                      clip_dist_table$distance[i], ",",
-                      "'", Sys.time(), "', ",
-                      "'", Sys.time(), "')",
-                      sep = "")
-}
-txt <- paste(txt, collapse = ", ")
-txt <- paste("insert into clip_dist (id, clip_id, sim_clip_id, distance, created_at, updated_at) values", txt, sep = "")
-dbGetQuery(con, txt)
-
-# dbListTables(con)
+# write everything into Postgres
+dbGetQuery(con, name_txt)
+dbGetQuery(con, prob_txt)
+dbGetQuery(con, dist_txt)
